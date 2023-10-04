80e70184aee3afac1d535350b1a035076cdd1688
ComputerCow <48889860+StinkyMilo@users.noreply.github.com>
2023-10-03 22:44:10 -0400

    the very basics are done

--- FinalProject/TestWebApp/wwwroot/js/shader.js
+++ FinalProject/TestWebApp/wwwroot/js/shader.js
@@ -11,7 +11,7 @@ function resizeCanvas() {
     canv.width = window.innerWidth;
     canv.height = window.innerHeight;
 };
-window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
 const ctx = canv.getContext("webgpu");
 const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
 ctx.configure({
@@ -59,15 +59,17 @@ code: `
     
     @fragment
     fn fs(@builtin(position) pos: vec4f) -> @location(0) vec4f{
-        let idx = u32((pos.y%res.y)*res.x + (res.x * -0.5) + pos.x%res.x);
+        // let npos = pos.xy/res;
+        // return vec4f(npos,0.,1.);
+        let idx = u32((pos.y%res.y)*res.x + pos.x%res.x);
         let X = cellState[idx].x;
         let Y = cellState[idx].y;
-        return vec4f(0,Y,Y,1.);
+        return vec4f(X,Y,0.,1.);
     }
 `
 });
 
-const WORKGROUP_SIZE = 16;
+const WORKGROUP_SIZE = 8;
 
 const simulationShaderModule =device.createShaderModule({
 label: "Game of life shader",
@@ -88,19 +90,20 @@ code:`
     @compute
     @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE})
     fn cs(@builtin(global_invocation_id) cell: vec3u){
+        // return;
         var convolution: array<f32, 9> = array(0.05, 0.2, 0.05, 0.2, -1., 0.2, 0.05, 0.2, 0.05);
         let currentIndex = cellIndex(cell.x,cell.y);
         var gradient: vec2f = vec2f(0.,0.);
         for(var i: u32 = 0; i < 9; i++){
             let adjustedIndex = cellIndex(cell.x+(i%3)-1,cell.y+(i/3)-1);
             let lastVal = cellStateIn[adjustedIndex];
-            gradient+=lastVal;
+            gradient+=convolution[i]*lastVal;
         }
         let in = cellStateIn[currentIndex];
         let X = in.x;
         let Y = in.y;
         let XOut = X+(Dx*gradient.x - X*Y*Y+f*(1-X));
-        let YOut = Y+(Dy*gradient.y - X*Y*Y-(k+f)*Y);
+        let YOut = Y+(Dy*gradient.y + X*Y*Y-(k+f)*Y);
         cellStateOut[currentIndex]=vec2f(XOut,YOut);
     }
 `
@@ -109,18 +112,18 @@ code:`
 const bindGroupLayout = device.createBindGroupLayout({
 label: "Cell bind group layout",
 entries: [{
-    binding: 0,
-    visibility: GPUShaderStage.COMPUTE | GPUShaderStage.FRAGMENT,
-    buffer: {}
-},{
-    binding: 1,
-    visibility: GPUShaderStage.COMPUTE | GPUShaderStage.FRAGMENT,
-    buffer: {type: "read-only-storage"}
-},{
-    binding: 2,
-    visibility: GPUShaderStage.COMPUTE,
-    buffer: {type: "storage"}
-}]
+        binding: 0,
+        visibility: GPUShaderStage.COMPUTE | GPUShaderStage.FRAGMENT,
+        buffer: {}
+    },{
+        binding: 1,
+        visibility: GPUShaderStage.COMPUTE | GPUShaderStage.FRAGMENT,
+        buffer: {type: "read-only-storage"}
+    },{
+        binding: 2,
+        visibility: GPUShaderStage.COMPUTE | GPUShaderStage.FRAGMENT,
+        buffer: {type: "storage"}
+    }]
 });
 
 const pipelineLayout = device.createPipelineLayout({
@@ -162,7 +165,9 @@ const uniformBuffer = device.createBuffer({
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
 });
 
-const cellStateArray = new Uint32Array(window.innerWidth*window.innerHeight*2);
+device.queue.writeBuffer(uniformBuffer,0,uniformArray);
+
+const cellStateArray = new Float32Array(window.innerWidth*window.innerHeight*2);
 const cellStateStorage = [
     device.createBuffer({
         label: "Cell State A",
@@ -177,68 +182,73 @@ const cellStateStorage = [
 ];
 
 for(var i = 0; i < cellStateArray.length; i+=2){
-    //TODO: Initialize cell state array
+    var x = (i/2)%window.innerHeight;
+    var y = ((i/2)-x)/window.innerHeight;
+    var distX = window.innerWidth/2 - x;
+    var distY = window.innerHeight/2 - y;
+    var dist = Math.sqrt(distX*distX + distY*distY);
+    if(dist < 100){
+        cellStateArray[i]=0;
+        cellStateArray[i+1]=1;
+    }else{
+        cellStateArray[i]=1;
+        cellStateArray[i+1]=0;
+    }
 }
 
 device.queue.writeBuffer(cellStateStorage[0],0,cellStateArray);
 
-for(var i = 0; i < cellStateArray.length; i+=3){
-    cellStateArray[i] = 1;
-}
 
 device.queue.writeBuffer(cellStateStorage[1],0,cellStateArray);
 
-
-
 const bindGroups = [
-device.createBindGroup({
-    label: "Cell renderer bind group A",
-    layout: cellPipeline.getBindGroupLayout(0),
-    entries: [{
-        binding: 0,
-        resource: {buffer: uniformBuffer}
-    },
-    {
-        binding: 1,
-        resource: {buffer: cellStateStorage[0]}
-    },{
-        binding: 2,
-        resource: {buffer: cellStateStorage[1]}
-    }]
-}),
+    device.createBindGroup({
+        label: "Cell renderer bind group A",
+        layout: cellPipeline.getBindGroupLayout(0),
+        entries: [{
+            binding: 0,
+            resource: {buffer: uniformBuffer}
+        },
+        {
+            binding: 1,
+            resource: {buffer: cellStateStorage[0]}
+        },{
+            binding: 2,
+            resource: {buffer: cellStateStorage[1]}
+        }]
+    }),
 
-device.createBindGroup({
-    label: "Cell renderer bind group B",
-    layout: cellPipeline.getBindGroupLayout(0),
-    entries :[{
-    binding: 0,
-        resource: { buffer: uniformBuffer}
-    },{
-    binding: 1,
-        resource: {buffer: cellStateStorage[1]}
-    },{
-    binding: 2,
-        resource: {buffer: cellStateStorage[0]}
-    }]
-})
+    device.createBindGroup({
+        label: "Cell renderer bind group B",
+        layout: cellPipeline.getBindGroupLayout(0),
+        entries :[{
+        binding: 0,
+            resource: { buffer: uniformBuffer}
+        },{
+        binding: 1,
+            resource: {buffer: cellStateStorage[1]}
+        },{
+        binding: 2,
+            resource: {buffer: cellStateStorage[0]}
+        }]
+    })
 ];
 
-device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
-
-const UPDATE_INTERVAL = 200;
+const UPDATE_INTERVAL = 20;
 let step =0;
+
+const numGroups = WORKGROUP_SIZE;
+const workgroupCount = [
+    Math.round(window.innerWidth/numGroups),
+    Math.round(window.innerHeight/numGroups),
+    1
+];
 
 function updateGrid(){
     const encoder = device.createCommandEncoder();
     const computePass = encoder.beginComputePass();
     computePass.setPipeline(simulationPipeline);
     computePass.setBindGroup(0, bindGroups[step%2]);
-    const numGroups = WORKGROUP_SIZE;
-    const workgroupCount = [
-        Math.round(window.innerWidth/numGroups),
-        Math.round(window.innerHeight/numGroups),
-        1
-    ];
     computePass.dispatchWorkgroups(workgroupCount[0], workgroupCount[1]);
     computePass.end();
     step++;
@@ -255,7 +265,7 @@ function updateGrid(){
     pass.setPipeline(cellPipeline);
     pass.setVertexBuffer(0,vertexBuffer);
     pass.setBindGroup(0, bindGroups[step%2]);
-    pass.draw(vertices.length/2, window.innerWidth*window.innerHeight);
+    pass.draw(vertices.length/2, 5);
 
     pass.end();
     const commandBuffer = encoder.finish();
